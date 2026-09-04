84e8517d0a83ab69e8477773aa9b99379d16e2ce
Jose Gomez De La Torre <jgomez@kirby.org>
2026-09-04 15:55:40 -0700

    Add initial structure for assignment problems

---
+++ assignment.py
@@ -0,0 +1,43 @@
+# The # symbol is the comment symbol for python, meaning anything after it won't be treated as code.
+#  Type you name her: 
+
+# Problem 1: Creating Initials 
+# Given 2 variables first_name and last_name, use indexing to print the initials in uppercase
+# strings have a .uppper() method that capitalizes all character    Ex "ab".upper() -> "AB" 
+first_name = "Patrick"
+last_name = "Star"
+
+
+
+
+
+# Problem 2: Sandwhich String
+# Given an outer string and word string, create a new string with the word in the middle of the outer string
+# Print the new string
+outer = "<<<>>>"
+word = "Cold"
+
+
+
+
+
+# Problem 3: Switching sides
+# Given a string, create a new string with the last 2 characters swapped with the beginnning 2 characters 
+# You can only use string indexing and + concatenation
+switch = "chocolate" # expected result: teocolach
+
+
+
+
+
+# Problems 4: Typo Fixer
+# Given a string, where a letter is missing. Create a new string with the missing letter added back in
+# You cannout use .replace(), only use index and concatenation
+missing = "infor_ation"
+missing_letter = "m"
+
+
+
+
+
+# Problem 5: 
